7bdefd320872fe7381e1add074c2fbc1a3b86754
Francia-bdz <francia.bidzimou@mmibordeaux.com>
2024-04-08 17:14:05 +0200

    feat : add suggestion to activity

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,11 +3,29 @@ class UsersController < ApplicationController
 
     protected
     
-    # 
     def ensure_admin
       redirect_to root_path unless current_user.admin?
     end
+ 
+    def index
+        @users = User.all
+    end
 
+    def show
+    end 
+
+    def update  
+        if @user.update(user_params)
+            redirect_to @user, notice: 'Le compte a bien été mis à jour.'
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @user.destroy
+        redirect_to users_url, notice: 'Le compte a bien été supprimé.'
+    end
 
 
 end
