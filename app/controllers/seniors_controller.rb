8ec72020e47be8f00f80a5361147507d392ae929
Francia-bdz <francia.bidzimou@mmibordeaux.com>
2024-04-10 17:54:16 +0200

    feat : simple form

--- app/controllers/seniors_controller.rb
+++ app/controllers/seniors_controller.rb
@@ -14,6 +14,7 @@ class SeniorsController < ApplicationController
 
   # GET /seniors/new
   def new
+  
     @senior = Senior.new
   end
 
--- app/models/participation.rb
+++ app/models/participation.rb
@@ -1,4 +1,9 @@
 class Participation < ApplicationRecord
   has_many :seniors
   has_many :activities
+  belongs_to :senior
+  belongs_to :activity
+
+
+
 end
